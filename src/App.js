b64697d91eee0def7e3df218db7af057673c39ba
Paulina Babinczuk <paulina.babinczuk@gmail.com>
2021-01-26 14:02:21 +0100

    implementing routing and adding navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import MainLayout from '../src/components/layout/MainLayout/MainLayout';
+import Login from '../src/components/views/Login/Login';
+import Tables from '../src/components/views/Tables/Tables';
+import Waiter from '../src/components/views/Waiter/Waiter';
+import Kitchen from '../src/components/views/Kitchen/Kitchen';
+import Home from '../src/components/views/Home/Home';
+import TablesBooking from './components/views/TablesBooking/TablesBooking';
+import TablesEvent from './components/views/TablesEvent/TablesEvent';
+import OrderNew from './components/views/OrderNew/OrderNew';
+import OrderSingle from './components/views/OrderSingle/OrderSingle';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={'/panel'}>
+      <MainLayout>
+        <Switch>
+          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
+          <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={TablesBooking} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables/event/:id'} component={TablesEvent} />
+          <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
+          <Route exact path={process.env.PUBLIC_URL + '/waiter/new'} component={OrderNew} />
+          <Route exact path={process.env.PUBLIC_URL + '/waiter/order/:id'} component={OrderSingle} />
+          <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+        </Switch>
+      </MainLayout>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/layout/PageNav/PageNav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './PageNav.scss'
+
+const PageNav = () => (
+    <nav>
+        <div>
+            <NavLink exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active' className={styles.navLink}>Home</NavLink>
+        </div>
+        <div>
+            <NavLink to={`${process.env.PUBLIC_URL}/login`} activeClassName='active' className={styles.navLink}>Login</NavLink>
+            <NavLink exact to={`${process.env.PUBLIC_URL}/waiter`} activeClassName='active' className={styles.navLink}>Waiter</NavLink>
+            <NavLink to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName='active' className={styles.navLink}>Kitchen</NavLink>
+            <NavLink exact to={`${process.env.PUBLIC_URL}/tables`} activeClassName='tables' className={styles.navLink}>Tables</NavLink>
+        </div>
+    </nav>
+);
+
+export default PageNav;
